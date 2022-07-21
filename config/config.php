7d7b2ef834e070fa855bd7f86981b607cab77211
Julio Cesar <97842753+ojuliocesar@users.noreply.github.com>
2022-07-21 01:04:51 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -11,8 +11,8 @@ if (ENVIRONMENT === 'development') {
 } else {
   define('DB_HOST', 'localhost');
   define('DB_PORT', '3306');
-  define('DB_NAME', 'db_ecommerce');
-  define('DB_USER', 'root');
-  define('DB_PASS', '');
+  define('DB_NAME', 'u668278917_db_ecommerce');
+  define('DB_USER', 'u668278917_ecommerce');
+  define('DB_PASS', '>BB[eITiz5N');
   define('DB_TABLE', 'product');
 }
