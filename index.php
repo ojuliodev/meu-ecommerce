bf10b00ac4a682c9f4645a25a72363c207d3bc8a
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-07-22 11:15:42 -0300

    Add customer page

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ ini_set("display_errors", 1);
 session_start();
 
 require_once('config/environment.php');
-
-$page = $_GET['page'];
+ 
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
 $action = isset($_GET['action']) ? $_GET['action'] : 'main';
 ?>
 
