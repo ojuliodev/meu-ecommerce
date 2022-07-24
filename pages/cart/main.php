99cb7b64beff25e976dac7d549f669495a4d50e1
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-07-24 15:35:43 -0300

    Fix cart responsive

--- pages/cart/main.php
+++ pages/cart/main.php
@@ -33,7 +33,7 @@ if (isset($_SESSION['cart']) && isset($_SESSION['customer'])) {
                 <th class="table-title product-title table-first">Produto</th>
                 <th class="table-title product-title table-quantity">Quantidade</th>
                 <th class="table-title product-title table-value">Valor</th>
-                <th class="table-title product-title table-remove">Remover</th>
+                <th class="table-title product-title table-remove">Quantidade</th>
             </tr>
         </thead>
         
